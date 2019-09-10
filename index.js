0ff7d3667c2503f0aeb4cd7061158f1cd512d83a
Brent Waters <bwaters2@republicservices.com>
2019-09-10 12:20:32 -0700

    cleaned up code, estimated usage of logging stream

--- index.js
+++ index.js
@@ -6,39 +6,25 @@ var masheryLogStreamUrl =  process.env.URL;
 const client = new WebSocket(masheryLogStreamUrl);
  
 client.onmessage = e => {
-
     handleDataEvent(e)
-
 }
-
 
 function handleDataEvent(e){
     
    var dataArray = JSON.parse(e.data.toString('utf8')).data
    
-    //add the data to the event log
+    //handle each data item for the event
     dataArray.forEach( function(data){
-      
-        var dataToLog = {     
-            'remote_total_time' :  data.remote_total_time,
-            'src_ip' : data.src_ip,
-            'api_method_name' : data.api_method_name,
-            'service_name' : data.service_name,
-            'uri' : data.uri
-        }
-        
         //mask any api keys before they are logged
         data.api_key = data.api_key=='unknown'?'unknown':'*masked*'
         
         //Some simple logic to filter out non-interesting events
         if( data.response_string!='200_OK_API' | data.traffic_manager_error_code!='-'){  
-            console.log(data)
-            writeToSplunkLog(data);
+            //logToConsole(data)
+            writeToSplunkLog(data)
         }
-        }
-    )
+    })
 }
-
 
 function writeToSplunkLog(parsedData){
 
@@ -51,9 +37,25 @@ function writeToSplunkLog(parsedData){
     var formattedData = '' 
     Object.keys(parsedData).forEach(function(key){formattedData += key+'='+parsedData[key] + ' '})
     
-    //fs.appendFile('masheryLog.log', '\n' + JSON.stringify(parsedData, null, 2) + '\n', function(err){console.log(err)})
     //For now mimic an empty thread context and [main]
-    fs.appendFile('masheryLog.log',   timeStamp + ' {} [main] ' +  formattedData + '\n', function(err){console.log(err)})
+    fs.appendFile('masherystream.log',   timeStamp + ' {} [main] ' +  formattedData + '\n', 
+                    function(err){if(err!=null){console.log(err)}}
+                 )
     
 
+}
+
+const startTime = new Date();
+var numberOfEvents = 0;
+function logToConsole(data){
+       var dataToLog = {     
+            'remote_total_time' :  data.remote_total_time,
+            'src_ip' : data.src_ip,
+            'api_method_name' : data.api_method_name,
+            'service_name' : data.service_name,
+            'uri' : data.uri
+        }
+        numberOfEvents++;
+        console.log("number of events handled: " + numberOfEvents + "\n TotalTime:" + ((startTime.getTime() - (new Date()).getTime())/(-1000)))
+        console.log(data)   
 }
